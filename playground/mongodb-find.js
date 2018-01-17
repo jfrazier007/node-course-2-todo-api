7812bd7f8480622b10c5b63b7e67e8f87698ccee
Jeff Frazier <jefffrazier@Supervisors-MBP.attlocal.net>
2018-01-16 23:25:05 -0500

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,39 @@
+// const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient,  ObjectID} = require('mongodb');
+
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db)=>{
+	if(err){
+		return console.log('Unable to connect to MondoDB server.');
+	}
+	console.log('Connected to MongoDB server.');
+
+	// db.collection('Todos').find({
+	// 	_id: new ObjectID('5a5ec67779a4320e2b52ee1b')
+	// }).toArray().then((docs)=>{
+	// 	console.log('Todos');
+	// 	console.log(JSON.stringify(docs, undefined, 2))
+	// }, (err)=>{
+	// 	console.log('Unable to fetch todos.', err);
+	// });
+
+	// db.collection('Todos').find().count().then((count)=>{
+	// 	console.log('Todos');
+	// 	console.log(`Todos count: ${count}`);
+	// }, (err)=>{
+	// 	console.log('Unable to fetch todos.', err);
+	// });
+
+	db.collection('Users').find({
+		name: 'Jeff Frazier'
+	}).count().then((count)=>{
+		console.log('Users with name = Jeff Frazier');
+		console.log(`equal ${count}`);
+	}, (err)=>{
+		console.log('Unable to fetch todos.', err);
+	});
+
+	db.close();
+});
